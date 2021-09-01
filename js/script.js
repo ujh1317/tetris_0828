258dc45c394dde2a25b000686d5d527b89eb6c80
ujh1317 <64245601+ujh1317@users.noreply.github.com>
2021-09-01 21:37:18 +0900

    block shape

--- js/script.js
+++ js/script.js
@@ -24,3 +24,26 @@ function makeBlock(col, row) {
         ALL_TD.push(X_array);
     }
 }
+
+
+var NOW_BLOCK = new Object();
+NOW_BLOCK.shape = null;  //현재 블록 형태
+NOW_BLOCK.current_blocks = null;  //현재 블록의 위치를 저장
+$(document).ready(function(){
+    //메모리 적재 시기에 작동
+    NOW_BLOCK.shape = 0;
+    displayBlock(NOW_BLOCK);
+});
+
+function displayBlock(nowblock) {  //원하는 블럭(nowblock)을 원하는 위치에 출력
+    //ALL_BLOCK[nowblock.shape]
+    for(var i=0; i < ALL_BLOCK[nowblock.shape][BLOCK_POSITION_SHAPE].length; i++) {
+        // ALL_BLOCK[nowblock.shape][0]  ->   "0:0"
+        var block_xy = ALL_BLOCK[nowblock.shape][BLOCK_POSITION_SHAPE][i].split(":");
+        var block_x  = Number(block_xy[0]) + 5;
+        var block_y  = Number(block_xy[1]) + 1;
+
+        $("#" + block_x + "_" + block_y).attr("class", "block");
+    }
+    
+}
